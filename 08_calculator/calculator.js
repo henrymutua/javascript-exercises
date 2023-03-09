c6d3273cce5d5374352994dd19a672e66ecd7e21
henry mutua <mutuahenry116@gmail.com>
2023-03-09 23:45:17 +0300

    Calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function(a, b, ...args) {
+  let sum = a+b;
+  for(const arg of args){
+    sum +=arg;
+  }
+	return sum;
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function(a, b, ...args) {
+  let diff = a-b;
+  for(const arg of args){
+    diff -=arg;
+  }
+	return diff;
 
 };
 
-const power = function() {
-	
+const sum = function(a, b, ...args) {
+  let addition = a+b;
+  for(const arg of args){
+    addition +=arg;
+  }
+	return addition;
 };
 
-const factorial = function() {
-	
+const multiply = function(a, b, ...args) {
+  let multi = a*b;
+  for(const arg of args){
+    multi*=arg;
+  }
+  return multi;
+};
+
+const power = function(a, b, ...args) {
+  let powerfunc = a**b;
+  for(const arg of args){
+    powerfunc**=arg;
+  }
+	return powerfunc;
+};
+
+const factorial = function(...args) {
+  if(args == 0 || args ==1)
+  return 1;
+  if(args<0)
+  return -1;
+  for(var i = args-1; i>=1; i--){
+    args = args*i;
+  }
+	return args;
 };
 
 // Do not edit below this line
